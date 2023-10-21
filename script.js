07af84b1c4ec86f2500ed963d91a37aa439541e3
David <davidgreene95@gmail.com>
2023-10-21 15:05:02 +0100

    Added game func with for loop to iterate game 5 times

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 //  Array of choices per game
 let arr = ["Rock", "Paper", "Scissors"];
 
+const computerSelection = getComputerChoice();
+const playerSelection = "Rock";
+
 // Function that will randomly pick between items in arr variable on every run
 function getComputerChoice() {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
 // Function for computer vs player that declares if round is one or lost
-function game() {
+function playRound(playerSelection, computerSelection) {
   // Input for player selection
   playerSelection = prompt("Rock, Paper, or Scissors?").toUpperCase();
 
@@ -41,7 +44,15 @@ function game() {
       return "Draw!";
     }
   }
+  // return result();
   console.log(result());
+}
+
+// Function with for loop to play five rounds and record previous scores
+function game() {
+  for (i = 0; i < 5; i++) {
+    playRound();
+  }
 }
 
 game();
