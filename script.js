f0e89580af63b7dddd11f6e5be5404c59b0fe128
David <david1.greene@intel.com>
2023-10-23 14:50:16 +0100

    Expanded on game func to included keeping score

--- script.js
+++ script.js
@@ -49,61 +49,25 @@ function getPlayerChoice() {
 
 // Function to play 5 games and display results
 function game() {
+  let scorePlayer = 0;
+  let scoreComputer = 0;
   for (let i = 0; i < 5; i++) {
     const playerSelection = getPlayerChoice();
     const computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
+    if (checkWinner(playerSelection, computerSelection) == "Player") {
+      scorePlayer++;
+    } else if (checkWinner(playerSelection, computerSelection) == "Computer") {
+      scoreComputer++;
+    }
+  }
+  if (scorePlayer > scoreComputer) {
+    console.log(`Player is the winner with ${scorePlayer} points!`);
+  } else if (scoreComputer > scorePlayer) {
+    console.log(`Computer is the winner with ${scorePlayer} points!`);
+  } else {
+    console.log("It's a tie!");
   }
 }
 
 game();
-// // Function for computer vs player that declares if round is one or lost
-// function playRound(playerSelection, computerSelection) {
-//   // Input for player selection
-//   playerSelection = prompt("Rock, Paper, or Scissors?").toUpperCase();
-//   console.log(playerSelection);
-
-//   // Computer selection
-//   computerSelection = getComputerChoice().toLowerCase();
-//   console.log(computerSelection);
-
-//   // If statement for result of game
-//   if (computerSelection === "ROCK" && playerSelection === "PAPER") {
-//     return Win;
-//   } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
-//     return Win;
-//   } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
-//     return Win;
-//   } else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
-//     return Lose;
-//   } else if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
-//     return Lose;
-//   } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
-//     return Lose;
-//   } else if (computerSelection === playerSelection) {
-//     return Draw;
-//   }
-// }
-// // Function with for loop to play five rounds and record previous scores
-
-// let player = 0;
-// let computer = 0;
-// let equal = 0;
-// let Win = "Win";
-// let Lose = "Lose";
-// let Draw = "Draw";
-
-// // console.log(playRound());
-
-// function playGame() {
-//   for (let i = 0; i < 5; i++) {
-//     console.log(playRound());
-//     if (playRound() === Win) {
-//       player++;
-//     } else if (playRound() === Lose) {
-//       computer++;
-//     }
-//   }
-// }
-
-// playGame();
